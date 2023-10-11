1ed13297db28d91ff1ee43ec684050118bf104c0
AshnahKhalidKhan <a.khan.22889@khi.iba.edu.pk>
2023-10-11 07:36:05 +0500

    Wohooooo Gridding

--- lib/Widgets/CodeLab2Grids.dart
+++ lib/Widgets/CodeLab2Grids.dart
@@ -36,7 +36,45 @@ class _CodeLab2GridsState extends State<CodeLab2Grids>
           ),
         ],
       ),
-      // body: 
+      body: GridView.count
+      (
+        crossAxisCount: 2,
+        padding: const EdgeInsets.all(16.0),
+        childAspectRatio: 8.0 / 9.0,
+        children: 
+        [
+          Card
+          (
+            clipBehavior: Clip.antiAlias,
+            child: Column
+            (
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>
+              [
+                AspectRatio
+                (
+                  aspectRatio: 18.0 / 11.0,
+                  child: Image.asset('lib/Assets/logo.jpg'),
+                ),
+                Padding
+                (
+                  padding: const EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
+                  child: Column
+                  (
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>
+                    [
+                      Text('Title'),
+                      const SizedBox(height: 8.0),
+                      Text('Secondary Text'),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      )
     );
   }
 }
