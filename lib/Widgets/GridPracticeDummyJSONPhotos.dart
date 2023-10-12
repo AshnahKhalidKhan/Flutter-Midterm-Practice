aaba7d0856103c40e1eb12d7e1ad5eb5f4350ac1
AshnahKhalidKhan <a.khan.22889@khi.iba.edu.pk>
2023-10-12 15:32:50 +0500

    We're practicing stuff baby

---
+++ lib/Widgets/GridPracticeDummyJSONPhotos.dart
@@ -0,0 +1,51 @@
+// {
+//   "products":
+//   [
+//     {
+//       "id":1,
+//       "title":"iPhone 9",
+//       "description":"An apple mobile which is nothing like apple",
+//       "price":549,
+//       "discountPercentage":12.96,
+//       "rating":4.69,
+//       "stock":94,
+//       "brand":"Apple",
+//       "category":"smartphones",
+//       "thumbnail":"https://i.dummyjson.com/data/products/1/thumbnail.jpg",
+//       "images":
+//         [
+//           "https://i.dummyjson.com/data/products/1/1.jpg",
+//           "https://i.dummyjson.com/data/products/1/2.jpg",
+//           "https://i.dummyjson.com/data/products/1/3.jpg",
+//           "https://i.dummyjson.com/data/products/1/4.jpg",
+//           "https://i.dummyjson.com/data/products/1/thumbnail.jpg"
+//         ]
+//       },
+//   ]
+// }
+
+class Gadgets
+{
+  final String title;
+  final String description;
+  final int price;
+  final double rating;
+  final String brand;
+  final String category;
+  final String thumbnail;
+  final List<String> images;
+
+  const Gadgets
+  (
+    {
+      required this.title,
+      required this.description,
+      required this.price,
+      required this.rating,
+      required this.brand,
+      required this.category,
+      required this.thumbnail,
+      required this.images,
+    }
+  );
+}
